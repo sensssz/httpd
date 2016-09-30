8a13e0d25ca9377a6cc09d9cc8f6354f38eef8b3
sensssz <hjmsens@gmail.com>
2016-09-30 14:57:56 -0400

    Try %pp

--- server/util_filter.c
+++ server/util_filter.c
@@ -601,7 +601,7 @@ AP_DECLARE(apr_status_t) ap_pass_brigade(ap_filter_t *next,
             }
         }
         clock_gettime(CLOCK_REALTIME, &call_start);
-        ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, NULL, "out_func is %p address",
+        ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, NULL, "out_func is %pp address",
                      ((void *) next->frec->filter_func.out_func));
         AP_DECLARE(apr_status_t) result = next->frec->filter_func.out_func(next, bb);
         clock_gettime(CLOCK_REALTIME, &call_end);
