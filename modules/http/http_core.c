a5c321c6674d6493f8a7564e752322dfd53ebd3c
sensssz <hjmsens@gmail.com>
2016-09-30 02:43:59 -0400

    Trace ap_process_async_request

--- modules/http/http_core.c
+++ modules/http/http_core.c
@@ -148,18 +148,13 @@ static int ap_process_http_async_connection(conn_rec *c)
 
         if ((r = ap_read_request(c))) {
             SESSION_START();
-            TRACE_FUNCTION_START();
             c->keepalive = AP_CONN_UNKNOWN;
             /* process the request if it was read without error */
 
             if (r->status == HTTP_OK) {
                 cs->state = CONN_STATE_HANDLER;
-                TRACE_START();
                 ap_update_child_status(c->sbh, SERVER_BUSY_WRITE, r);
-                TRACE_END(1);
-                TRACE_START();
                 ap_process_async_request(r);
-                TRACE_END(2);
                 /* After the call to ap_process_request, the
                  * request pool may have been deleted.  We set
                  * r=NULL here to ensure that any dereference
@@ -174,10 +169,8 @@ static int ap_process_http_async_connection(conn_rec *c)
                 cs->state != CONN_STATE_SUSPENDED) {
                 /* Something went wrong; close the connection */
                 cs->state = CONN_STATE_LINGER;
-                TRACE_FUNCTION_END();
                 SESSION_END(0);
             } else {
-                TRACE_FUNCTION_END();
                 SESSION_END(1);
             }
         }
