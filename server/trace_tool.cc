0758b8ae0f6cbd1bca9496c247d98b3b74ab8dfa
sensssz <hjmsens@gmail.com>
2016-10-01 14:57:23 -0400

    Trace basic_http_header

--- server/trace_tool.cc
+++ server/trace_tool.cc
@@ -18,8 +18,8 @@ using std::string;
 using std::to_string;
 using std::set;
 
-#define TARGET_PATH_COUNT 2
-#define NUMBER_OF_FUNCTIONS 6
+#define TARGET_PATH_COUNT 3
+#define NUMBER_OF_FUNCTIONS 1
 #define LATENCY
 #define MONITOR
 
