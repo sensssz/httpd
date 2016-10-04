a84812c5f43b3322af8f711e5eb556d06e95e723
sensssz <hjmsens@gmail.com>
2016-10-03 21:24:09 -0400

    Set memory to 1G.

--- server/trace_tool.cc
+++ server/trace_tool.cc
@@ -131,7 +131,7 @@ __thread timespec TraceTool::trans_start;
 bool TraceTool::should_shutdown = false;
 pthread_t TraceTool::back_thread;
 
-const long MAX_SIZE = 1.5 * 1024 * 1024 * 1024;
+const long MAX_SIZE = 1 * 1024 * 1024 * 1024;
 static char *memory = (char *) malloc(MAX_SIZE * sizeof(char));
 static long offset = 0;
 
