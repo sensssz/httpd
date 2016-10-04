842ed73e26c9e428047101db7f8715d7cf0e2944
sensssz <hjmsens@gmail.com>
2016-10-04 10:40:04 -0400

    Remove alloc calls.

--- server/trace_tool.cc
+++ server/trace_tool.cc
@@ -132,7 +132,7 @@ __thread timespec TraceTool::trans_start;
 bool TraceTool::should_shutdown = false;
 pthread_t TraceTool::back_thread;
 
-const long MAX_SIZE = 128 * 1024 * 1024;
+const long MAX_SIZE = 512 * 1024 * 1024;
 thread_local char *memory = (char *) malloc(MAX_SIZE * sizeof(char));
 thread_local long offset = 0;
 
